e27f09f35dc6fa40faa1fec2d416a881b1b77dc5
JHPolarBear <jhpark8904@gmail.com>
2021-02-21 18:54:36 +0900

    Add macro log

--- Util/StringUtility.cpp
+++ Util/StringUtility.cpp
@@ -2,6 +2,8 @@
 
 #include "StringUtility.h"
 
+
+
 using namespace std;
 
 namespace Util
--- Util/StringUtility.h
+++ Util/StringUtility.h
@@ -3,6 +3,14 @@
 #include <vector>
 #include <string>
 
+#include <stdio.h>
+
+#define __FILENAME__	(strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+
+#define FILE_LOG(fmt, ...)	{printf("[%s:%d / %s] "fmt"\n", __FILENAME__, __LINE__, __FUNCTION__, ##__VA_ARGS__);} 
+
+#define FUNC_LOG(fmt, ...)	{printf("[%s] "fmt"\n", __FUNCTION__, ##__VA_ARGS__);} 
+
 namespace Util
 {
 	/** divide string with delimeter and save into int vector **/
